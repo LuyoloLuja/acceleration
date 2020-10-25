a83951ea5483d6a5deb7f452be34b4917cf1e29f
LuyoloLuja <luyolom19@gmail.com>
2020-10-25 02:48:35 +0200

    added my factory functions

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const bodyParser = require('body-parser');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const Nandis = require("./nandi");
-const nandis = Nandis();
-
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:luyolo@localhost:5432/nandi';
 
 const pool = new Pool({
   connectionString
 });
+
+const Nandis = require("./nandi");
+const nandis = Nandis(pool);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -27,22 +28,45 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res){
   const {name, email, message} = req.body;
 
-  let feedback = nandis.customer(name, email, message);
+  let feedback = nandis.customerEnquiery(name, email, message);
   res.render('home', {
     feedback
 });
 })
+app.get('/cart', function(req, res){
+  let price = women_dresses.price;
+
+
+  res.render('addToCart', {
+    price
+  })
+})
+
+// app.post('/cart', async function(req, res){
+//   const {dress, size, quantity} = req.body;
+//   let productChosen = await nandis.buyDresses(size, quantity, dress);
+//   console.log(size, quantity, dress);
+
+//   res.render('addToCart', {
+//     productChosen
+//   })
+// })
+
+// app.post('/', async function(req, res){
+//   const {dress, size, quantity} = req.body;
+//   let productChosen = await nandis.buyDresses(size, quantity, dress);
+
+//   res.render('addToCart', {
+//     productChosen
+//   })
+// })
 
 app.get('/shop', function(req, res){
   res.render('shop')
 })
-app.post('')
 
 app.get('/form', function(req, res){
   res.render('form')
-})
-app.get('/cart', function(req, res){
-  res.render('addToCart')
 })
 
 let PORT = process.env.PORT || 3030;
--- nandi.js
+++ nandi.js
@@ -1,31 +1,39 @@
 module.exports = function Nandis(pool){
 
-    async function selectProduct(product){
-        if(product){
-            let setProduct = await pool.query('SELECT product_name FROM products WHERE product_name = $1', [product]);
-
-            if(setProduct.rowCount === 0){
-                await pool.query('INSERT INTO products (product_name) VALUES ($1)', [product])
-            }else {
-                await pool.query('UPDATE products SET product_price = product_price + product_price WHERE product_name = $1', [product])
+    async function buyDresses(dress, size, price){
+        if(dress, size, price){
+            // selecting dress_number === which dress(foreign key),
+            let selectDress = await pool.query('SELECT dress_number FROM women_dresses_info WHERE dress_number = $1', [dress]);
+            let dressSize = await pool.query('SELECT size FROM women_dresses_info WHERE size = $1', [size]);
+            let dressPrice = await pool.query('SELECT women_dresses.price FROM women_dresses WHERE women_dresses.price = $1', [price]);
+            
+            if(selectDress.rowCount !== 0 && dressSize.rowCount !== 0 && dressPrice.rowCount !== 0) {
+                await pool.query(`UPDATE women_dresses_info SET women_dresses.price = women_dresses.price + women_dresses.price
+                                WHERE women_dresses_info.price = $1`, [price]);
             }
         }
     }
 
-    async function getSelectedProducts(){
-        let products = await pool.query('SELECT product_name FROM products');
-        return products.rows;
+    async function customerProducts(customerName,product){
+        if(customerName,product){
+            await pool.query('INSERT INTO customer (customer_name, customer_product) VALUES (customerName, product)');
+        }
     }
 
-    function customer(name, email, message){
+    async function itemsAbove(){
+        await pool.query('SELECT dress_number FROM women_dresses_info WHERE women_dresses.price ON women_dresses > 300');
+    }
+
+    function customerEnquiery(name, email, message){
         if(name, email, message){
-            return "Thank you. We have received your enquiry, we'll get back to you!"
+            return "Thank you. We have received your enquiry, we'll get back to you!";
         }
     }
 
     return {
-        customer,
-        selectProduct,
-        getSelectedProducts
+        buyDresses,
+        itemsAbove,
+        customerEnquiery,
+        customerProducts
     }
 }
